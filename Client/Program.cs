d329e2c8dff94c1e8ae5eef1e362efcce5a49f3a
Krzysztof Wróblewski <szawro@gmail.com>
2020-04-18 22:14:53 +0200

    Init

---
+++ Client/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Blazored.LocalStorage;
+
+namespace KrzyWro.CAH.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBaseAddressHttpClient();
+
+            builder.Services.AddBlazoredLocalStorage();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
