8de8113be40a7949c47f84d4c91a130fee7229bb
Krzysztof Wróblewski <kontakt@krzywro.pl>
2021-01-15 22:51:56 +0100

    Podbicie do .NET 5.0

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,9 +13,9 @@ namespace KrzyWro.CAH.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredLocalStorage();
 
             builder.Services.AddScoped<IPlayerHubClient, PlayerHubClient>();
---
+++ Server/Hubs/GameHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KrzyWro.CAH.Server.Hubs
+{
+    public class GameHub : Hub
+    {
+    }
+}
