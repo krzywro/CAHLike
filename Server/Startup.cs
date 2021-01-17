1c2a019b5541d590660e0f033403ef88fcc1c1fd
Krzysztof Wróblewski <kontakt@krzywro.pl>
2021-01-17 22:04:22 +0100

    Lekkie zmany pod reverse=proxy

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -48,10 +48,13 @@ namespace KrzyWro.CAH.Server
             {
                 app.UseDeveloperExceptionPage();
                 app.UseWebAssemblyDebugging();
+                app.UseForwardedHeaders();
+
             }
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseForwardedHeaders();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
