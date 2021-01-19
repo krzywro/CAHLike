a3a85469f95a91fdd87c61714a784278ab804608
Krzysztof Wróblewski <kontakt@krzywro.pl>
2021-01-19 18:43:43 +0100

    Przypadkowe wyrzucenie o 1 linijkę za dużo

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -26,6 +26,7 @@ namespace KrzyWro.CAH.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
             services.AddSignalR();
             services.AddDistributedMemoryCache();
             services.AddScoped<IPlayerPoolService, PlayerPoolService>();
