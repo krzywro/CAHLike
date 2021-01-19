4ab7ba28035a72370cc2188a6d48af781b8ebc8e
Krzysztof Wróblewski <kontakt@krzywro.pl>
2021-01-19 18:25:38 +0100

    Wyłączenie auto-przekierowania na HTTPS

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -26,8 +26,6 @@ namespace KrzyWro.CAH.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllersWithViews();
             services.AddSignalR();
             services.AddDistributedMemoryCache();
             services.AddScoped<IPlayerPoolService, PlayerPoolService>();
@@ -59,7 +57,6 @@ namespace KrzyWro.CAH.Server
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
